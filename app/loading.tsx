19bde6d1a37317e14d45ce23dbf1369f59f4c00a
Kavin Jey <jeyakavin33@gmail.com>
2024-07-19 12:07:20 -0400

    Feature x (#21)
    * some changes here
    
    * logging
    
    ---------
    
    Co-authored-by: Kavin Jey <kavinjey@Kavins-MacBook-Pro.local>

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,6 +2,11 @@ import { Title, Text } from '@tremor/react';
 import Search from './search';
 
 export default function Loading() {
+
+    console.log("this is a change here")
+    console.log("this is a change here")
+    console.log("this is a change here")
+    console.log("this is a change here")
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <Title>Users</Title>

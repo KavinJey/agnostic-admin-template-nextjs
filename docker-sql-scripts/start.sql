7b38b1ee925dc7218a266922a8ba659bb1623224
Kavin Jey <kavinjey@Kavins-MacBook-Pro.local>
2024-07-19 10:57:15 -0400

    new changes to destroy prod

--- docker-sql-scripts/start.sql
+++ docker-sql-scripts/start.sql
@@ -6,3 +6,4 @@ CREATE TABLE users (
 );
 
 INSERT INTO users (id, email, name, username) VALUES (1, 'me@site.com', 'Me', 'username');
+INSERT INTO users (id, email, name, username) VALUES (1, 'me2@site.com', 'Me2', 'username');
